61db070363d2d5f0c314ae6c7c03891e1e157b60
Maikell Cruz <maikell.cruz99@gmail.com>
2023-03-11 16:12:42 -0300

    Teste 01

---
+++ src/test/java/com/example/DeliverySpring/DeliverySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DeliverySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
